3846097fed9fa58b026063257ddfa40ac76d6bc8
Hennadii Yarosh <nedinix.tech@gmail.com>
2023-05-10 23:11:58 +0300

    markup-v2

--- src/index.js
+++ src/index.js
@@ -1,24 +1,27 @@
-import './js/refs';
-
+import refs from './js/refs';
 import axios from 'axios';
 import { renderGallery } from './js/render-main-gallery';
-
-
 
 const BASE_URL = 'https://books-backend.p.goit.global/books/top-books';
 
 async function mainGallery() {
-    try {
-        const response = await axios.get(BASE_URL);
-        console.log(response);
-        renderGallery(response.data);
-    } catch (error) {
-        console.log(error);
-    }
+  try {
+    const response = await axios.get(BASE_URL);
+    console.log(response);
+    renderGallery(response.data);
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 mainGallery();
 
 // import './js/test';
 
+refs.userLoginBtn.addEventListener('click', toggleAuthForm);
+refs.authFormCloseBtn.addEventListener('click', toggleAuthForm);
 
+function toggleAuthForm() {
+  document.body.classList.toggle('.auth-form-open');
+  refs.authFormBackdrop.classList.toggle('is-hidden');
+}
--- src/js/refs.js
+++ src/js/refs.js
@@ -1,5 +1,7 @@
 export default {
   mainGalleryEl: document.querySelector('.container-main-gallery'),
   userLoginBtn: document.querySelector('.user-login'),
+  authFormBackdrop: document.querySelector('.auth-backdrop'),
+  authFormCloseBtn: document.querySelector('.auth-form-close-btn'),
   regFormEl: document.querySelector('.auth-form'),
 };
