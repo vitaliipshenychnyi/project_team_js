0381bb7961720c5bd6b2a0900c2098d24061215c
Hennadii Yarosh <nedinix.tech@gmail.com>
2023-05-10 21:30:08 +0300

    markup

--- src/js/refs.js
+++ src/js/refs.js
@@ -1,4 +1,5 @@
 export default {
   mainGalleryEl: document.querySelector('.container-main-gallery'),
   userLoginBtn: document.querySelector('.user-login'),
+  regFormEl: document.querySelector('.auth-form'),
 };
