6aee2bcf50f4ef8ec97222829d2c76ac78281f87
VitaliiPshenychniy <4kvaap@gmail.com>
2023-05-10 22:31:58 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,25 +1,19 @@
 // import './js/test'; ПШЕНИЧНИЙ (не чипати)
 import './js/refs';
-import './js/test';
-import './js/categories-list'
+import './js/categories-list';
 import axios from 'axios';
 import { renderGallery } from './js/render-main-gallery';
-
-
 
 const BASE_URL = 'https://books-backend.p.goit.global/books/top-books';
 
 async function mainGallery() {
-    try {
-        const response = await axios.get(BASE_URL);
-        console.log(response.data);
-        renderGallery(response.data);
-    } catch (error) {
-        console.log(error);
-    }
+  try {
+    const response = await axios.get(BASE_URL);
+    console.log(response.data);
+    renderGallery(response.data);
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 mainGallery();
-
-
-
