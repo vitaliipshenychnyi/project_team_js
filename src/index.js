b999dafcc63cc72df7d61e4181800904312d8bd9
Vanchenko <vanchenkoev@pakline.com.ua>
2023-05-10 19:56:56 +0300

    commit1

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
 import './js/refs';
+import axios from 'axios';
+import { renderGallery } from './js/render-main-gallery';
+
+
+
+const BASE_URL = 'https://books-backend.p.goit.global/books/top-books';
+
+async function mainGallery() {
+    try {
+        const response = await axios.get(BASE_URL);
+        console.log(response);
+        renderGallery(response.data);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+mainGallery();
