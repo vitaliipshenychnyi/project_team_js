2cbbe684779e236dd4ec614a340c2123f753cabc
VitaliiPshenychniy <4kvaap@gmail.com>
2023-05-10 22:26:24 +0300

    Update categories-list.js

--- src/js/categories-list.js
+++ src/js/categories-list.js
@@ -1,17 +1,17 @@
-import { getCategoriesList } from "./test";
-import { createMarkupCategoriesList } from "./test";
-import { refs } from "./refs";
+// import { getCategoriesList } from "./test";
+// import { createMarkupCategoriesList } from "./test";
+// import { refs } from "./refs";
 
-// рендеринг списку категорій
-getCategoriesList().then(async resp => {
-    const categories = resp.data;
-    const listFillingError = '<p class="categories - err">The list of categories is empty</p>';
-    console.log(categories);
-        if (categories.length === 0) {
-            refs.categoriesList.innerHTML = listFillingError;
-                return;
-        }
-    refs.categoriesList.innerHTML = createMarkupCategoriesList(categories);
-    }).catch(err => {
-        catList.innerHTML = listFillingError;
-    })
+// // рендеринг списку категорій
+// getCategoriesList().then(async resp => {
+//     const categories = resp.data;
+//     const listFillingError = '<p class="categories - err">The list of categories is empty</p>';
+//     console.log(categories);
+//         if (categories.length === 0) {
+//             refs.categoriesList.innerHTML = listFillingError;
+//                 return;
+//         }
+//     refs.categoriesList.innerHTML = createMarkupCategoriesList(categories);
+//     }).catch(err => {
+//         catList.innerHTML = listFillingError;
+//     })
