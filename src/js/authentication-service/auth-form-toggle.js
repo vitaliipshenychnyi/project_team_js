9c247347b9c4f7c9093b68697be260b1f1059aaf
Hennadii Yarosh <nedinix.tech@gmail.com>
2023-05-11 00:04:37 +0300

    fixed and refactoring

---
+++ src/js/authentication-service/auth-form-toggle.js
@@ -0,0 +1,11 @@
+import refs from '../refs';
+
+export default function onLoginBtn() {
+  refs.userLoginBtn.addEventListener('click', toggleAuthForm);
+  refs.authFormCloseBtn.addEventListener('click', toggleAuthForm);
+}
+
+function toggleAuthForm() {
+  document.body.classList.toggle('.auth-form-open');
+  refs.authFormBackdrop.classList.toggle('is-hidden');
+}
