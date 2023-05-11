8c01e3191a91ab383d3a779826c4c6ac533598f2
VitaliiPshenychniy <4kvaap@gmail.com>
2023-05-11 16:11:48 +0300

    style_1

--- src/js/render-main-gallery.js
+++ src/js/render-main-gallery.js
@@ -2,14 +2,31 @@ import refs from './refs';
 
 function renderRowGallery(row) {
   return row
-    .map(
-      elem =>
-        `<div class="book-card">
-                <a class="gallery link" href="${elem.amazon_product_url}">
-                    <img src="${elem.book_image}" alt="" loading="lazy"/> 
-                    <p class"book-name">${elem.title}</p>
-                </a>
-            </div>`
+    .map(elem =>
+      elem.title.length < 20
+        ? `<div class="book-card-wrapper"><a class="overlay link" href="${elem.amazon_product_url}">
+            <img src="${elem.book_image}" alt="${elem.title}" height="256" loading="lazy"/> 
+            <div class="overlay-field">
+              <p class="overlay-text">QUICK VIEW</p>
+            </div>
+            <p class="book-name">${elem.title}</p>
+            <p class="book-author">${elem.author}</p>
+          </a></div>`
+        : `<div class="book-card-wrapper"><a class="overlay link" href="${
+            elem.amazon_product_url
+          }">
+            <img src="${elem.book_image}" alt="${
+            elem.title
+          }" height="256" loading="lazy"/> 
+            <div class="overlay-field">
+              <p class="overlay-text">QUICK VIEW</p>
+            </div>
+            <p class="book-name">${elem.title
+              .split('')
+              .slice(0, 20)
+              .join('')}...</p>
+            <p class="book-author">${elem.author}</p>
+          </a></div>`
     )
     .join('');
 }
@@ -20,11 +37,13 @@ export function renderGallery(books) {
   markUp = books
     .map(
       elem =>
-        `<p>${elem.list_name}</p>
-        <div class="book-category">
-            ${renderRowGallery(elem.books)}
-        </div>
-        <button class="button-see-more" type="button">SEE MORE</button>`
+        `<div class="books-category">
+          <p class="books-category-title">${elem.list_name}</p>
+          <ul">
+            <li class="books__list">${renderRowGallery(elem.books)}</li>
+          </ul>
+          <button class="button-see-more" type="button">SEE MORE</button>
+        </div>`
     )
     .join('');
   refs.mainGalleryEl.insertAdjacentHTML('beforeend', markUp);
