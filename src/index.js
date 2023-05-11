53a9a674fea5cc90f74d6e732fb48eb436eefe56
Liza <lizapikovskaya@gmail.com>
2023-05-11 12:38:19 +0300

    make markup for mobile menu and script for it

--- src/index.js
+++ src/index.js
@@ -3,5 +3,5 @@ import './js/refs';
 import './js/categories-list';
 import './js/API-main-gallary';
 import onLoginBtn from './js/authentication-service/auth-form-toggle';
-
+import './js/mobile-menu';
 onLoginBtn();
--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -1,25 +1,25 @@
-// const burgerBtnEl = document.querySelector('.js-open-mobile-menu');
-// const mobileDivEl = document.querySelector('.js-mobile-menu');
-// const iconOpenMobileMenu = document.querySelector('.icon-burger-mobile-menu');
-// const iconCloseMobileMenu = document.querySelector('.icon-close-mobile-menu');
+const burgerBtnEl = document.querySelector('.js-open-mobile-menu');
+const mobileDivEl = document.querySelector('.js-mobile-menu');
+const iconOpenMobileMenu = document.querySelector('.icon-burger-mobile-menu');
+const iconCloseMobileMenu = document.querySelector('.icon-close-mobile-menu');
 
-// burgerBtnEl.addEventListener('click', onOpenMobileMenu);
+burgerBtnEl.addEventListener('click', onOpenMobileMenu);
 
-// function onOpenMobileMenu(evt) {
-//   if (iconCloseMobileMenu.classList.contains('is-hidden')) {
-//     showMobileMenu();
-//     iconOpenMobileMenu.classList.add('is-hidden');
-//     iconCloseMobileMenu.classList.remove('is-hidden');
-//   } else {
-//     hideMobileMenu();
-//     iconOpenMobileMenu.classList.remove('is-hidden');
-//     iconCloseMobileMenu.classList.add('is-hidden');
-//   }
-// }
+function onOpenMobileMenu(evt) {
+  if (iconCloseMobileMenu.classList.contains('is-hidden')) {
+    showMobileMenu();
+    iconOpenMobileMenu.classList.add('is-hidden');
+    iconCloseMobileMenu.classList.remove('is-hidden');
+  } else {
+    hideMobileMenu();
+    iconOpenMobileMenu.classList.remove('is-hidden');
+    iconCloseMobileMenu.classList.add('is-hidden');
+  }
+}
 
-// function showMobileMenu() {
-//   mobileDivEl.classList.remove('is-hidden-modal');
-// }
-// function hideMobileMenu() {
-//   mobileDivEl.classList.add('is-hidden-modal');
-// }
+function showMobileMenu() {
+  mobileDivEl.classList.remove('is-hidden-modal');
+}
+function hideMobileMenu() {
+  mobileDivEl.classList.add('is-hidden-modal');
+}
