ff7b3e14cabf3d6f2bfcf7ef83072e565afad28d
Vanchenko <vanchenkoev@pakline.com.ua>
2023-05-11 15:09:40 +0300

    render catgallery

--- src/js/render-main-gallery.js
+++ src/js/render-main-gallery.js
@@ -29,3 +29,31 @@ export function renderGallery(books) {
     .join('');
   refs.mainGalleryEl.insertAdjacentHTML('beforeend', markUp);
 }
+
+function renderRowGalleryCat(row,r1,r2,r3) {
+    return `<div class="book-card">
+                <a class="gallery link" href="${r1}">
+                    <img src="${row}" alt="" loading="lazy"/> 
+                    <p class"book-name">${r2}</p>
+                    <p class"book-name">${r3}</p>
+                </a>
+            </div>`;
+}
+
+export function renderGalleryCat(books, cat) {
+    let markUp = `<h2>${cat}</h2>
+                <div class="book-category wrap">`;
+   // console.log(markUp);
+    console.log(books);
+    markUp = markUp + books.map(elem =>
+        `<div class="book-card">
+            <a class="gallery link" href="${elem.amazon_product_url}">
+                <img src="${elem.book_image}" alt="" loading="lazy"/> 
+                <p class"book-name">${elem.title}</p>
+                <p class"book-name">${elem.author}</p>
+            </a>
+        </div>`).join('');
+    markUp = markUp + `</div>`
+    console.log(markUp);
+    refs.mainGalleryEl.insertAdjacentHTML('beforeend', markUp);
+}
