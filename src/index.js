9f6584dfc20a6d114e8b3e546309e79a0b5f1cc2
Liza <lizapikovskaya@gmail.com>
2023-05-11 20:55:14 +0300

    dark-theme

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import './js/API-main-gallary';
 import onLoginBtn from './js/authentication-service/auth-form-toggle';
 import './js/api-firebase';
 import './js/mobile-menu';
+import './js/togle-theme';
 
 onLoginBtn();
 
