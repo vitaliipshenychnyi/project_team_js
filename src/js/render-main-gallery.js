6d0907ac0d7c6eeb6f4d3ed83d37891d73d2f48e
Vanchenko <vanchenkoev@pakline.com.ua>
2023-05-12 11:39:29 +0300

    поправил стили в категориях

--- src/js/render-main-gallery.js
+++ src/js/render-main-gallery.js
@@ -21,10 +21,7 @@ function renderRowGallery(row) {
             <div class="overlay-field">
               <p class="overlay-text">QUICK VIEW</p>
             </div>
-            <p class="book-name">${elem.title
-              .split('')
-              .slice(0, 20)
-              .join('')}...</p>
+            <p class="book-name">${elem.title.split('').slice(0, 20).join('')}...</p>
             <p class="book-author">${elem.author}</p>
           </a></div>`
     )
@@ -33,19 +30,16 @@ function renderRowGallery(row) {
 
 export function renderGallery(books) {
   let markUp = '';
-  console.log(books);
+  //console.log(books);
   markUp = books
-    .map(
-      elem =>
+    .map(elem =>
         `<div class="books-category">
           <p class="books-category-title">${elem.list_name}</p>
           <ul">
             <li class="books__list">${renderRowGallery(elem.books)}</li>
           </ul>
           <button class="button-see-more" type="button" data-cat="${elem.list_name}">SEE MORE</button>
-        </div>`
-    )
-    .join('');
+        </div>`).join('');
   refs.mainGalleryEl.insertAdjacentHTML('beforeend', markUp);
 }
 
@@ -54,15 +48,35 @@ export function renderGalleryCat(books, cat) {
     refs.mainGalleryTitleEl.textContent = `${cat}`;
     let markUp = "";
    // console.log(markUp);
-    console.log(books);
-    markUp = books.map(elem =>
-        `<div class="book-card">
-            <a class="gallery link" href="${elem.amazon_product_url}">
+    //console.log(books);
+  markUp = books.map(elem =>
+    elem.title.length < 20
+      ? `<div class="book-card-wrapper"><a class="overlay link" href="${elem.amazon_product_url}">
+            <img src="${elem.book_image}" alt="${elem.title}" height="256" loading="lazy"/> 
+            <div class="overlay-field">
+              <p class="overlay-text">QUICK VIEW</p>
+            </div>
+            <p class="book-name">${elem.title}</p>
+            <p class="book-author">${elem.author}</p>
+          </a></div>`
+      : `<div class="book-card-wrapper"><a class="overlay link" href="${elem.amazon_product_url
+      }">
+            <img src="${elem.book_image}" alt="${elem.title
+      }" height="256" loading="lazy"/> 
+            <div class="overlay-field">
+              <p class="overlay-text">QUICK VIEW</p>
+            </div>
+            <p class="book-name">${elem.title.split('').slice(0, 20).join('')}...</p>
+            <p class="book-author">${elem.author}</p>
+          </a></div>`
+  ).join('');
+       /* `<div class="book-card-wrapper">
+            <a class="overlay link" href="${elem.amazon_product_url}">
                 <img src="${elem.book_image}" alt="" loading="lazy"/> 
                 <p class"book-name">${elem.title}</p>
-                <p class"book-name">${elem.author}</p>
+                <p class"book-author">${elem.author}</p>
             </a>
-        </div>`).join('');
+        </div>`).join('');*/
     //console.log(markUp);
     refs.mainGalleryEl.classList.add("gal-category");
     refs.mainGalleryEl.insertAdjacentHTML('beforeend', markUp);
