4cf9aeca9234b71e54f2ccc82b1c52e87150284e
Vanchenko <vanchenkoev@pakline.com.ua>
2023-05-12 14:20:42 +0300

    исправил Title категории

--- src/js/refs.js
+++ src/js/refs.js
@@ -1,6 +1,7 @@
 export default {
     mainGalleryEl: document.querySelector('.container-main-gallery'),
-    mainGalleryTitleEl: document.querySelector('.category-title'),
+  mainGalleryTitleEl: document.querySelector('.category-title'),
+  mainGalleryTitleSpanEl: document.querySelector('.category-title-span'),
   categoriesList: document.querySelector('.categories-list'),
   userLoginBtn: document.querySelector('.user-login'),
   authFormBackdrop: document.querySelector('.auth-backdrop'),
