e5c8a0c7b311d4a15e1c609bf81cd6f4ae6d2310
Liza <lizapikovskaya@gmail.com>
2023-05-14 17:09:33 +0300

    import img to function, change toggle color for footer

--- src/js/render-photo-card-footer.js
+++ src/js/render-photo-card-footer.js
@@ -1,56 +1,66 @@
+import imgMatcipura from '/src/img/card-photo/andriymatcipura@2x.jpg';
+import imgPshenychnyi from '/src/img/card-photo/vitaliypshenychnyi@2x.jpg';
+import imgLuzina from '/src/img/card-photo/julialuzina@2x.jpg';
+import imgEnvanchenko from '/src/img/card-photo/evgenvanchenko@2x.jpg';
+import imgDolyuk from '/src/img/card-photo/vitaliydolyuk@2x.jpg';
+import imgPikovska from '/src/img/card-photo/lizapikovska@2x.jpg';
+import imgBreslavets from '/src/img/card-photo/olexanderbreslavets@2x.jpg';
+import imgYarosh from '/src/img/card-photo/hennadiyyarosh@2x.jpg';
+import imgRusnak from '/src/img/card-photo/ivanrusnak@2x.jpg';
+
 const cards = [
   {
     id: 1,
     link: 'https://github.com/vitaliipshenychnyi',
-    src: './img/card-photo/vitaliypshenychnyi@2x.jpg',
+    src: imgPshenychnyi,
     alt: 'vitaliypshenychnyi',
   },
   {
     id: 2,
     link: 'https://github.com/WOWmasterJulia',
-    src: './img/card-photo/julialuzina@2x.jpg',
+    src: imgLuzina,
     alt: 'julialuzina',
   },
   {
     id: 3,
     link: 'https://github.com/vitaliipshenychnyi',
-    src: './img/card-photo/evgenvanchenko@2x.jpg',
+    src: imgEnvanchenko,
     alt: 'evgenvanchenko',
   },
   {
     id: 4,
     link: 'https://github.com/Doluvit',
-    src: './img/card-photo/vitaliydolyuk@2x.jpg',
+    src: imgDolyuk,
     alt: 'vitaliydolyuk',
   },
   {
     id: 5,
     link: 'https://github.com/Matsypura',
-    src: './img/card-photo/andriymatcipura@2x.jpg',
+    src: imgMatcipura,
     alt: 'andriymatcipura',
   },
   {
     id: 6,
     link: 'https://github.com/LizaPikovskaya',
-    src: './img/card-photo/lizapikovska@2x.jpg',
+    src: imgPikovska,
     alt: 'lizapikovska',
   },
   {
     id: 7,
     link: 'https://github.com/AleksandrBreslavets',
-    src: './img/card-photo/olexanderbreslavets@2x.jpg',
+    src: imgBreslavets,
     alt: 'olexanderbreslavets',
   },
   {
     id: 8,
     link: 'https://github.com/nedinix',
-    src: './img/card-photo/hennadiyyarosh@2x.jpg',
+    src: imgYarosh,
     alt: 'hennadiyyarosh',
   },
   {
     id: 9,
     link: '#',
-    src: './img/card-photo/ivanrusnak@2x.jpg',
+    src: imgRusnak,
     alt: 'ivanrusnak',
   },
 ];
@@ -58,16 +68,22 @@ const cards = [
 const groupBtnEl = document.querySelector('.button-group-logo');
 const cardListEl = document.querySelector('.card-list');
 const checkIconEl = document.querySelector('.group-logo-icon');
+const teamParEl = document.querySelector('.our-team')
 groupBtnEl.addEventListener('click', onRollingOutClick);
 
 function onRollingOutClick(evt) {
   if (cardListEl.childElementCount) {
     checkIconEl.classList.remove('return-check');
-    cardListEl.classList.remove('animation-appear');
+    // cardListEl.classList.remove('animation-appear');
+    cardListEl.classList.add('is-hidden');
+    teamParEl.classList.add('is-hidden');
+
     cardListEl.innerHTML = '';
   } else {
     checkIconEl.classList.add('return-check');
-    cardListEl.classList.add('animation-appear');
+    // cardListEl.classList.add('animation-appear');
+    teamParEl.classList.remove('is-hidden');
+        cardListEl.classList.remove('is-hidden');
     cardListEl.innerHTML = createMArkUp(cards);
   }
 }
