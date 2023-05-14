10ae9c405f52ec4df9ba11142e0d2dfb9b0f3397
Matsypura <117666348+Matsypura@users.noreply.github.com>
2023-05-14 21:20:47 +0300

    support11

--- src/js/support.js
+++ src/js/support.js
@@ -8,49 +8,50 @@ import img6 from '/src/img/support-icon/save@2x.png';
 import img7 from '/src/img/support-icon/serhiy@2x.png';
 import img8 from '/src/img/support-icon/united24@2x.png';
 import img9 from '/src/img/support-icon/world@2x.png';
-
+import svgdown from '/src/img/svg/svgdown.svg';
+import svgup from '/src/img/svg/svgup.svg';
 
 const icons = [
   { id: 1,  title: 'Save the Children',
    url: 'https://www.savethechildren.net/what-we-do/emergencies/ukraine-crisis', 
    icon: img1,
-   position: 1,
+   position: "01",
    className: 'icon' },
   { id: 2, title: 'Project HOPE',
    url: 'https://www.projecthope.org/country/ukraine/',
-    icon: img2, position: 2,
-    className: 'icon' },
+    icon: img2, position: "02",
+    className: 'icon', width: 129 },
   { id: 3,  title: 'UNITED24', url: 'https://u24.gov.ua/uk',
    icon: img3,
-    position: 3,
+    position: "03",
     className: 'icon' },
   { id: 4,  title: 'International Medical Corps',
    url: 'https://internationalmedicalcorps.org/country/ukraine/', 
    icon: img4,
-    position: 4,
+    position: "04",
     className: 'icon' },
   { id: 5, title: 'Medicins Sans Frontieres',
    url:'https://www.msf.org/ukraine',
     icon: img5,
-     position: 5,
+     position: "05",
      className: 'icon'},
   { id: 6, title: 'RAZOM', url: 'https://www.razomforukraine.org/',
    icon: img6,
-    position: 6,
+    position: "06",
     className: 'icon' },
   { id: 7, title: 'Action against hunger',
    url:'https://www.actionagainsthunger.org/location/europe/ukraine/',
     icon: img7,
-     position: 7,
+     position: "07",
      className: 'icon' },
   { id: 8, title: 'World vision', url: 'https://www.wvi.org/emergencies/ukraine',
    icon: img8,
-    position: 8,
+    position: "08",
     className: 'icon' },
   { id: 9, title: 'Serhiy Prytula Charity Foundation', 
   url: 'https://prytulafoundation.org/en', 
   icon: img9, 
-  position: 9,
+  position: "09",
   className: 'icon' },
 ];
 
@@ -61,6 +62,72 @@ if (window.innerWidth >= 360 && window.innerWidth <= 767) {
   itemsQuery = 4;
 }
 
+
+function renderRowGallery(icons) {
+
+
+  const markup = icons
+    .slice(0, SUPPORT_ITEMS_QUERY)
+    .map(
+      elem =>
+        `
+        <a class = "support-link"  href="${elem.url}">
+          <p class ="support-p">${elem.position}</p>
+          <img src="${elem.icon}" class = "support-item"   alt="" loading="lazy"/> 
+         
+        </a>
+       
+      `
+    )
+    .join('');
+  
+  refs.supportRenderEl.innerHTML = markup;
+
+}
+
+
+
+
+const iconsShow = icons.slice(0, itemsQuery);
+let isChangeIcon = true;
+renderRowGallery(iconsShow);
+refs.supportLoadBtn.addEventListener('click', onClick);
+function onClick() {
+
+
+
+  if (isChangeIcon) {
+    if (iconsShow[iconsShow.length - 1].id == 9) {
+      return isChangeIcon = false;
+    }
+    const indexLastElement = icons.findIndex((element) => element.id === iconsShow[iconsShow.length - 1].id);
+    iconsShow.push(icons[indexLastElement + 1]);
+    iconsShow.shift(); // delete first element
+  } else {
+    if (iconsShow[0].id === 1) {
+      return isChangeIcon = true;
+    }
+    const indexFirstElement = icons.findIndex((element) => element.id === iconsShow[0].id);
+    iconsShow.unshift(icons[indexFirstElement - 1]);
+    iconsShow.pop(); // delete last element
+  }
+
+  if (iconsShow[0].id === 4 || iconsShow[0].id === 1) {
+    isChangeIcon = !isChangeIcon;
+  }
+
+  renderRowGallery(iconsShow);
+
+  if(isChangeIcon){
+    refs.iconSvgBtnEl.innerHTML = `<use href= "${svgdown}"></use>`;
+// вниз
+  }else{
+   
+    refs.iconSvgBtnEl.innerHTML = `<use href= "${svgup}"></use>`;
+// вверх
+  }
+console.log(refs.iconSvgBtnEl)
+}
 // function renderRowGallery(icons) {
 //   const container = document.createElement('div');
 
@@ -88,70 +155,3 @@ if (window.innerWidth >= 360 && window.innerWidth <= 767) {
 //   refs.supportRenderEl.innerHTML = '';
 //   refs.supportRenderEl.appendChild(container);
 // }
-function renderRowGallery(icons) {
-
-// console.log(itemsQuery)
-  const markup = icons
-    .slice(0, SUPPORT_ITEMS_QUERY)
-    .map(
-      elem =>
-        `<div class="">
-        <a href="${elem.url}">
-          <p>${elem.id}</p>
-          <img src="${elem.icon}" alt="" loading="lazy"/> 
-         
-        </a>
-        
-      </div>`
-    )
-    .join('');
-  
-  refs.supportRenderEl.innerHTML = markup;
-  // console.log(refs.supportEl);
-}
-
-
-
-
-const iconsShow = icons.slice(0, itemsQuery);
-let isChangeIcon = true;
-renderRowGallery(iconsShow);
-refs.supportLoadBtn.addEventListener('click', onClick);
-function onClick() {
-
-
-// console.log(iconsShow)
-// console.log(isChangeIcon)
-  if (isChangeIcon) {
-    if (iconsShow[iconsShow.length - 1].id == 9) {
-      return isChangeIcon = false;
-    }
-    const indexLastElement = icons.findIndex((element) => element.id === iconsShow[iconsShow.length - 1].id);
-    iconsShow.push(icons[indexLastElement + 1]);
-    iconsShow.shift(); // delete first element
-  } else {
-    if (iconsShow[0].id === 1) {
-      return isChangeIcon = true;
-    }
-    const indexFirstElement = icons.findIndex((element) => element.id === iconsShow[0].id);
-    iconsShow.unshift(icons[indexFirstElement - 1]);
-    iconsShow.pop(); // delete last element
-  }
-
-  if (iconsShow[0].id === 4 || iconsShow[0].id === 1) {
-    isChangeIcon = !isChangeIcon;
-  }
-
-  renderRowGallery(iconsShow);
-
-//   if(isChangeIcon){
-//     refs.iconSvgBtnEl.innerHTML = `<svg class="support-icon" width="7" height="13"> <use href= "/src/img/svg/sprite.svg#icon-scroll-down"></use>  </svg>`;
-// // вниз
-//   }else{
-   
-//     refs.iconSvgBtnEl.innerHTML = `<svg class="support-icon" width="7" height="13"> <use href= "/src/img/svg/sprite.svg#icon-scroll-up"></use>  </svg>`;
-// // вверх
-//   }
-refs.iconSvgBtnEl.innerHTML = `<svg class="support-icon" width="7" height="13"> <use href= "/src/img/svg/sprite.svg#icon-scroll-${isChangeIcon ? 'down' : 'up'}"></use>  </svg>`;
-
-}
