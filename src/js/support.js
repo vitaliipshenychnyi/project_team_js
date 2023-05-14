6274e3bd2bd9eb8b400b6a3a8a36b20e48ef197e
VitaliiPshenychniy <4kvaap@gmail.com>
2023-05-14 19:46:36 +0300

    Update support.js

--- src/js/support.js
+++ src/js/support.js
@@ -1,57 +1,87 @@
-import refs from "./refs";
-import img1 from '/src/img/support-icon/action@2x.png';
-import img2 from '/src/img/support-icon/internationa@2x.png';
-import img3 from '/src/img/support-icon/medicins@2x.png';
-import img4 from '/src/img/support-icon/projectHOPE@2x.png';
-import img5 from '/src/img/support-icon/razom@2x.png';
-import img6 from '/src/img/support-icon/save@2x.png';
-import img7 from '/src/img/support-icon/serhiy@2x.png';
-import img8 from '/src/img/support-icon/united24@2x.png';
-import img9 from '/src/img/support-icon/world@2x.png';
-
+import refs from './refs';
+import img1 from '../img/support-icon/action@2x.png';
+import img2 from '../img/support-icon/internationa@2x.png';
+import img3 from '../img/support-icon/medicins@2x.png';
+import img4 from '../img/support-icon/projectHOPE@2x.png';
+import img5 from '../img/support-icon/razom@2x.png';
+import img6 from '../img/support-icon/save@2x.png';
+import img7 from '../img/support-icon/serhiy@2x.png';
+import img8 from '../img/support-icon/united24@2x.png';
+import img9 from '../img/support-icon/world@2x.png';
 
 const icons = [
-  { id: 1,  title: 'Save the Children',
-   url: 'https://www.savethechildren.net/what-we-do/emergencies/ukraine-crisis', 
-   icon: img1,
-   position: 1,
-   className: 'icon' },
-  { id: 2, title: 'Project HOPE',
-   url: 'https://www.projecthope.org/country/ukraine/',
-    icon: img2, position: 2,
-    className: 'icon' },
-  { id: 3,  title: 'UNITED24', url: 'https://u24.gov.ua/uk',
-   icon: img3,
+  {
+    id: 1,
+    title: 'Save the Children',
+    url: 'https://www.savethechildren.net/what-we-do/emergencies/ukraine-crisis',
+    icon: img1,
+    position: 1,
+    className: 'icon',
+  },
+  {
+    id: 2,
+    title: 'Project HOPE',
+    url: 'https://www.projecthope.org/country/ukraine/',
+    icon: img2,
+    position: 2,
+    className: 'icon',
+  },
+  {
+    id: 3,
+    title: 'UNITED24',
+    url: 'https://u24.gov.ua/uk',
+    icon: img3,
     position: 3,
-    className: 'icon' },
-  { id: 4,  title: 'International Medical Corps',
-   url: 'https://internationalmedicalcorps.org/country/ukraine/', 
-   icon: img4,
+    className: 'icon',
+  },
+  {
+    id: 4,
+    title: 'International Medical Corps',
+    url: 'https://internationalmedicalcorps.org/country/ukraine/',
+    icon: img4,
     position: 4,
-    className: 'icon' },
-  { id: 5, title: 'Medicins Sans Frontieres',
-   url:'https://www.msf.org/ukraine',
+    className: 'icon',
+  },
+  {
+    id: 5,
+    title: 'Medicins Sans Frontieres',
+    url: 'https://www.msf.org/ukraine',
     icon: img5,
-     position: 5,
-     className: 'icon'},
-  { id: 6, title: 'RAZOM', url: 'https://www.razomforukraine.org/',
-   icon: img6,
+    position: 5,
+    className: 'icon',
+  },
+  {
+    id: 6,
+    title: 'RAZOM',
+    url: 'https://www.razomforukraine.org/',
+    icon: img6,
     position: 6,
-    className: 'icon' },
-  { id: 7, title: 'Action against hunger',
-   url:'https://www.actionagainsthunger.org/location/europe/ukraine/',
+    className: 'icon',
+  },
+  {
+    id: 7,
+    title: 'Action against hunger',
+    url: 'https://www.actionagainsthunger.org/location/europe/ukraine/',
     icon: img7,
-     position: 7,
-     className: 'icon' },
-  { id: 8, title: 'World vision', url: 'https://www.wvi.org/emergencies/ukraine',
-   icon: img8,
+    position: 7,
+    className: 'icon',
+  },
+  {
+    id: 8,
+    title: 'World vision',
+    url: 'https://www.wvi.org/emergencies/ukraine',
+    icon: img8,
     position: 8,
-    className: 'icon' },
-  { id: 9, title: 'Serhiy Prytula Charity Foundation', 
-  url: 'https://prytulafoundation.org/en', 
-  icon: img9, 
-  position: 9,
-  className: 'icon' },
+    className: 'icon',
+  },
+  {
+    id: 9,
+    title: 'Serhiy Prytula Charity Foundation',
+    url: 'https://prytulafoundation.org/en',
+    icon: img9,
+    position: 9,
+    className: 'icon',
+  },
 ];
 
 const SUPPORT_ITEMS_QUERY = 6;
@@ -89,8 +119,7 @@ if (window.innerWidth >= 360 && window.innerWidth <= 767) {
 //   refs.supportRenderEl.appendChild(container);
 // }
 function renderRowGallery(icons) {
-
-// console.log(itemsQuery)
+  // console.log(itemsQuery)
   const markup = icons
     .slice(0, SUPPORT_ITEMS_QUERY)
     .map(
@@ -103,35 +132,34 @@ function renderRowGallery(icons) {
 `
     )
     .join('');
-  
+
   refs.supportRenderEl.innerHTML = markup;
   // console.log(refs.supportEl);
 }
-
-
-
 
 const iconsShow = icons.slice(0, itemsQuery);
 let isChangeIcon = true;
 renderRowGallery(iconsShow);
 refs.supportLoadBtn.addEventListener('click', onClick);
 function onClick() {
-
-
-// console.log(iconsShow)
-// console.log(isChangeIcon)
+  // console.log(iconsShow)
+  // console.log(isChangeIcon)
   if (isChangeIcon) {
     if (iconsShow[iconsShow.length - 1].id == 9) {
-      return isChangeIcon = false;
+      return (isChangeIcon = false);
     }
-    const indexLastElement = icons.findIndex((element) => element.id === iconsShow[iconsShow.length - 1].id);
+    const indexLastElement = icons.findIndex(
+      element => element.id === iconsShow[iconsShow.length - 1].id
+    );
     iconsShow.push(icons[indexLastElement + 1]);
     iconsShow.shift(); // delete first element
   } else {
     if (iconsShow[0].id === 1) {
-      return isChangeIcon = true;
+      return (isChangeIcon = true);
     }
-    const indexFirstElement = icons.findIndex((element) => element.id === iconsShow[0].id);
+    const indexFirstElement = icons.findIndex(
+      element => element.id === iconsShow[0].id
+    );
     iconsShow.unshift(icons[indexFirstElement - 1]);
     iconsShow.pop(); // delete last element
   }
@@ -142,14 +170,15 @@ function onClick() {
 
   renderRowGallery(iconsShow);
 
-//   if(isChangeIcon){
-//     refs.iconSvgBtnEl.innerHTML = `<svg class="support-icon" width="7" height="13"> <use href= "/src/img/svg/sprite.svg#icon-scroll-down"></use>  </svg>`;
-// // вниз
-//   }else{
-   
-//     refs.iconSvgBtnEl.innerHTML = `<svg class="support-icon" width="7" height="13"> <use href= "/src/img/svg/sprite.svg#icon-scroll-up"></use>  </svg>`;
-// // вверх
-//   }
-refs.iconSvgBtnEl.innerHTML = `<svg class="support-icon" width="7" height="13"> <use href= "/src/img/svg/sprite.svg#icon-scroll-${isChangeIcon ? 'down' : 'up'}"></use>  </svg>`;
+  //   if(isChangeIcon){
+  //     refs.iconSvgBtnEl.innerHTML = `<svg class="support-icon" width="7" height="13"> <use href= "/src/img/svg/sprite.svg#icon-scroll-down"></use>  </svg>`;
+  // // вниз
+  //   }else{
 
+  //     refs.iconSvgBtnEl.innerHTML = `<svg class="support-icon" width="7" height="13"> <use href= "/src/img/svg/sprite.svg#icon-scroll-up"></use>  </svg>`;
+  // // вверх
+  //   }
+  refs.iconSvgBtnEl.innerHTML = `<svg class="support-icon" width="7" height="13"> <use href= "/src/img/svg/sprite.svg#icon-scroll-${
+    isChangeIcon ? 'down' : 'up'
+  }"></use>  </svg>`;
 }
