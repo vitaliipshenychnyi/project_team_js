4deacc788d5b34689e60341d4c4b1706cf72a33b
Matsypura <117666348+Matsypura@users.noreply.github.com>
2023-05-14 19:22:44 +0300

    support 10

--- src/js/support.js
+++ src/js/support.js
@@ -1,14 +1,57 @@
 import refs from "./refs";
+import img1 from '/src/img/support-icon/action@2x.png';
+import img2 from '/src/img/support-icon/internationa@2x.png';
+import img3 from '/src/img/support-icon/medicins@2x.png';
+import img4 from '/src/img/support-icon/projectHOPE@2x.png';
+import img5 from '/src/img/support-icon/razom@2x.png';
+import img6 from '/src/img/support-icon/save@2x.png';
+import img7 from '/src/img/support-icon/serhiy@2x.png';
+import img8 from '/src/img/support-icon/united24@2x.png';
+import img9 from '/src/img/support-icon/world@2x.png';
+
+
 const icons = [
-  { id: 1,  title: 'Save the Children', url: 'https://www.savethechildren.net/what-we-do/emergencies/ukraine-crisis', icon: "../img/src/img/support-Icon/Action@1x.png", position: 1 },
-  { id: 2, title: 'Project HOPE', url: 'https://www.projecthope.org/country/ukraine/', icon: '/src/img/support-Icon/support1x/ProjectHOPE@1x.png', position: 2 },
-  { id: 3,  title: 'UNITED24', url: 'https://u24.gov.ua/uk', icon: '/src/img/support-Icon/support1x/UNITED24@1x.png', position: 3 },
-  { id: 4,  title: 'International Medical Corps', url: 'https://internationalmedicalcorps.org/country/ukraine/', icon: '/src/img/support-Icon/support1x/Internationa@1x.png', position: 4 },
-  { id: 5, title: 'Medicins Sans Frontieres', url: 'https://www.msf.org/ukraine', icon: '/src/img/support-Icon/support1x/Medicins@1x.png', position: 5 },
-  { id: 6, title: 'RAZOM', url: 'https://www.razomforukraine.org/', icon: '/src/img/support-Icon/support1x/RAZOM@1x.png', position: 6 },
-  { id: 7, title: 'Action against hunger', url: 'https://www.actionagainsthunger.org/location/europe/ukraine/', icon: '/src/img/support-Icon/support1x/Action@1x.png', position: 7 },
-  { id: 8, title: 'World vision', url: 'https://www.wvi.org/emergencies/ukraine', icon: '/src/img/support-Icon/support1x/World@1x.png', position: 8 },
-  { id: 9, title: 'Serhiy Prytula Charity Foundation', url: 'https://prytulafoundation.org/en', icon: '/src/img/support-Icon/support1x/Serhiy@1x.png', position: 9 },
+  { id: 1,  title: 'Save the Children',
+   url: 'https://www.savethechildren.net/what-we-do/emergencies/ukraine-crisis', 
+   icon: img1,
+   position: 1,
+   className: 'icon' },
+  { id: 2, title: 'Project HOPE',
+   url: 'https://www.projecthope.org/country/ukraine/',
+    icon: img2, position: 2,
+    className: 'icon' },
+  { id: 3,  title: 'UNITED24', url: 'https://u24.gov.ua/uk',
+   icon: img3,
+    position: 3,
+    className: 'icon' },
+  { id: 4,  title: 'International Medical Corps',
+   url: 'https://internationalmedicalcorps.org/country/ukraine/', 
+   icon: img4,
+    position: 4,
+    className: 'icon' },
+  { id: 5, title: 'Medicins Sans Frontieres',
+   url:'https://www.msf.org/ukraine',
+    icon: img5,
+     position: 5,
+     className: 'icon'},
+  { id: 6, title: 'RAZOM', url: 'https://www.razomforukraine.org/',
+   icon: img6,
+    position: 6,
+    className: 'icon' },
+  { id: 7, title: 'Action against hunger',
+   url:'https://www.actionagainsthunger.org/location/europe/ukraine/',
+    icon: img7,
+     position: 7,
+     className: 'icon' },
+  { id: 8, title: 'World vision', url: 'https://www.wvi.org/emergencies/ukraine',
+   icon: img8,
+    position: 8,
+    className: 'icon' },
+  { id: 9, title: 'Serhiy Prytula Charity Foundation', 
+  url: 'https://prytulafoundation.org/en', 
+  icon: img9, 
+  position: 9,
+  className: 'icon' },
 ];
 
 const SUPPORT_ITEMS_QUERY = 6;
@@ -18,6 +61,33 @@ if (window.innerWidth >= 360 && window.innerWidth <= 767) {
   itemsQuery = 4;
 }
 
+// function renderRowGallery(icons) {
+//   const container = document.createElement('div');
+
+//   icons.slice(0, SUPPORT_ITEMS_QUERY).forEach((elem) => {
+//     const div = document.createElement('div');
+//     const link = document.createElement('a');
+//     const paragraph1 = document.createElement('p');
+//     const image = document.createElement('img');
+//     const paragraph2 = document.createElement('p');
+
+//     link.href = elem.url;
+//     paragraph1.textContent = elem.id;
+//     image.src = elem.icon;
+//     image.alt = '';
+//     image.loading = 'lazy';
+//     paragraph2.textContent = elem.title;
+
+//     link.appendChild(paragraph1);
+//     link.appendChild(image);
+//     link.appendChild(paragraph2);
+//     div.appendChild(link);
+//     container.appendChild(div);
+//   });
+
+//   refs.supportRenderEl.innerHTML = '';
+//   refs.supportRenderEl.appendChild(container);
+// }
 function renderRowGallery(icons) {
 
 // console.log(itemsQuery)
@@ -29,7 +99,7 @@ function renderRowGallery(icons) {
         <a href="${elem.url}">
           <p>${elem.id}</p>
           <img src="${elem.icon}" alt="" loading="lazy"/> 
-          <p>${elem.title}</p>
+         
         </a>
         
       </div>`
@@ -40,7 +110,8 @@ function renderRowGallery(icons) {
   // console.log(refs.supportEl);
 }
 
-// console.log("nfjfjfjfjfjfj",itemsQuery)
+
+
 
 const iconsShow = icons.slice(0, itemsQuery);
 let isChangeIcon = true;
@@ -74,13 +145,13 @@ function onClick() {
   renderRowGallery(iconsShow);
 
 //   if(isChangeIcon){
-//     refs.iconSvgBtnEl.innerHTML = <svg class="support-icon" width="7" height="13"> <use href= "/src/img/svg/sprite.svg#icon-scroll-down"></use>  </svg>
+//     refs.iconSvgBtnEl.innerHTML = `<svg class="support-icon" width="7" height="13"> <use href= "/src/img/svg/sprite.svg#icon-scroll-down"></use>  </svg>`;
 // // вниз
 //   }else{
-//     refs.iconSvgBtnEl.innerHTML = <svg class="support-icon" width="7" height="13"> <use href= "/src/img/svg/sprite.svg#icon-scroll-up"></use>  </svg>
+   
+//     refs.iconSvgBtnEl.innerHTML = `<svg class="support-icon" width="7" height="13"> <use href= "/src/img/svg/sprite.svg#icon-scroll-up"></use>  </svg>`;
 // // вверх
 //   }
-
-
+refs.iconSvgBtnEl.innerHTML = `<svg class="support-icon" width="7" height="13"> <use href= "/src/img/svg/sprite.svg#icon-scroll-${isChangeIcon ? 'down' : 'up'}"></use>  </svg>`;
 
 }
