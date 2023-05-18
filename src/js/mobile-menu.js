004a953455c7fe97e510254b7e4c2b39e467bf86
Liza <lizapikovskaya@gmail.com>
2023-05-18 11:24:00 +0300

    block scroll while opening mobile-menu, add closing by escape, fix icon in Support Ukraine block

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -6,20 +6,27 @@ const iconCloseMobileMenu = document.querySelector('.icon-close-mobile-menu');
 burgerBtnEl.addEventListener('click', onOpenMobileMenu);
 
 function onOpenMobileMenu(evt) {
+  window.addEventListener('keydown', closeMenuByEsc);
+  function closeMenuByEsc(evt) {
+    if (evt.code === 'Escape') hideMobileMenu();
+    window.removeEventListener('keydown', closeMenuByEsc);
+  }
+
   if (iconCloseMobileMenu.classList.contains('is-hidden')) {
     showMobileMenu();
-    iconOpenMobileMenu.classList.add('is-hidden');
-    iconCloseMobileMenu.classList.remove('is-hidden');
   } else {
     hideMobileMenu();
-    iconOpenMobileMenu.classList.remove('is-hidden');
-    iconCloseMobileMenu.classList.add('is-hidden');
   }
 }
-
 function showMobileMenu() {
   mobileDivEl.classList.remove('is-hidden-modal');
+  iconOpenMobileMenu.classList.add('is-hidden');
+  iconCloseMobileMenu.classList.remove('is-hidden');
+  document.body.style.overflow = 'hidden';
 }
 function hideMobileMenu() {
   mobileDivEl.classList.add('is-hidden-modal');
+  iconOpenMobileMenu.classList.remove('is-hidden');
+  iconCloseMobileMenu.classList.add('is-hidden');
+  document.body.style.overflowY = '';
 }
